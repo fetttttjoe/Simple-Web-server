bd735da1f0ab00e5994123c8732ee195130fa103
SnapTrap <45294596+fetttttjoe@users.noreply.github.com>
2022-03-17 00:46:59 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -1,17 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, request, render_template, jsonify
 
-app = Flask(__name__)
+app = Flask("LazyTool")
 
-#root folder
-@app.route("/")
-def index():
-    return '<h1 style="text-align:center;color:red">This is a simple test to check if Flask is running Properly</h1'
-
+@app.route('/')
+def default():
+    return render_template('default.html')
+@app.route('/templates/frame.html', methods=['POST', 'GET'])
+def inputBox():
+    if request.method == "POST":
+        userInput = request.form['userInput']
+        print("Function userInput:",userInput)
+    return render_template('frame.html')
+    
+#testing purpose
 @app.route("/test_data")
 def names():
-    data = {"Random List": ["IDK", "Dave ist doof", "Wtf", "I can't care less"]}
+    data = {"Command List": ["Search Browser", "Open CMD", "Wtf", "I can't care less"]}
     return jsonify(data)
 
-
 if __name__ == "__main__":
-    app.run(ssl_context=("cert.pem", "key.pem"), debug=True) #prob not the way to go, but since its local i cant care less.
+    app.run("0.0.0.0", 5000, ssl_context=("cert.pem", "key.pem"), debug=True) #prob not the way to go, but since its local i cant care less.
