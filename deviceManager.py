4d1903a3425fe9dad9ea608fcd503de44298bc7d
SnapTrap <45294596+fetttttjoe@users.noreply.github.com>
2022-03-17 01:52:50 +0100

    we can give some limited input into the shittiest input field you ever saw!

---
+++ deviceManager.py
@@ -0,0 +1,98 @@
+#TODO: Clean this mess
+import win32gui
+import win32api
+import win32con
+import time
+
+VK_CODE = {
+    'tab': 0x09,
+    'clear': 0x0C,
+    'enter': 0x0D,
+    'shift': 0x10,
+    'ctrl': 0x11,
+    'delete': 0x2E,
+    'back': 0x08,
+    'esc' : 0x1B,
+    ' ' : 0x20,
+    '0': 0x30,
+    '1': 0x31,
+    '2': 0x32,
+    '3': 0x33,
+    '4': 0x34,
+    '5': 0x35,
+    '6': 0x36,
+    '7': 0x37,
+    '8': 0x38,
+    '9': 0x39,
+    'a': 0x41,
+    'b': 0x42,
+    'c': 0x43,
+    'd': 0x44,
+    'e': 0x45,
+    'f': 0x46,
+    'g': 0x47,
+    'h': 0x48,
+    'i': 0x49,
+    'j': 0x4A,
+    'k': 0x4B,
+    'l': 0x4C,
+    'm': 0x4D,
+    'n': 0x4E,
+    'o': 0x4F,
+    'p': 0x50,
+    'q': 0x51,
+    'r': 0x52,
+    's': 0x53,
+    't': 0x54,
+    'u': 0x55,
+    'v': 0x56,
+    'w': 0x57,
+    'x': 0x58,
+    'y': 0x59,
+    'z': 0x5A,
+    'F1': 0x70,
+    'F2': 0x71,
+    'F3': 0x72,
+    'F4': 0x73,
+    'F5': 0x74,
+    'F6': 0x75,
+    'F7': 0x76,
+    'F8': 0x77,
+    'F9': 0x78,
+    'F10': 0x79,
+    'F11': 0x7A,
+    'F12': 0x7B,
+    'F13': 0x7C,
+    'F14': 0x7D,
+    'F15': 0x7E,
+    'F16': 0x7F,
+    'F17': 0x80,
+    'F18': 0x81,
+    'F19': 0x82,
+    'F20': 0x83,
+    'F21': 0x84,
+    'F22': 0x85,
+    'F23': 0x86,
+    'F24': 0x87, }
+
+def mouseLeftlick(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+    time.sleep(.05)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+
+def mouseRightclick(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0)
+    time.sleep(.05)
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0)
+
+def getMousePosition():
+   return win32gui.GetCursorPos()
+
+def keyboardEvent(user_input):
+    win32api.keybd_event(VK_CODE[f'{user_input}'], 0, 0, 0)
+    time.sleep(.05)
+    win32api.keybd_event(VK_CODE[f'{user_input}'], 0, win32con.KEYEVENTF_KEYUP, 0)
+
+#dM.keyboardEvent(0x4F)
--- server.py
+++ server.py
@@ -1,4 +1,19 @@
 from flask import Flask, request, render_template, jsonify
+import deviceManager as dM
+
+def inputHandler(userInput):
+    temp = userInput.replace(' ', '')
+    if temp in dM.VK_CODE:
+        dM.keyboardEvent(userInput)
+        return 0
+    if temp.isalnum():
+        for element in userInput:
+            if element in dM.VK_CODE:
+                dM.keyboardEvent(element)
+        return 0
+    else:
+        return -1 #TODO: Better Error Handling 
+        
 
 app = Flask("LazyTool")
 
@@ -6,12 +21,18 @@ app = Flask("LazyTool")
 def default():
     return render_template('default.html')
 @app.route('/templates/frame.html', methods=['POST', 'GET'])
-def inputBox():
+def frame():
     if request.method == "POST":
         userInput = request.form['userInput']
         print("Function userInput:",userInput)
     return render_template('frame.html')
-    
+@app.route('/templates/inputBox.html', methods=['POST', 'GET'])
+def inputBox():
+    if request.method == "POST":
+        userInput = request.form['userInput']
+        inputHandler(userInput)
+        print("Function userInput:",userInput)
+    return render_template('inputBox.html')    
 #testing purpose
 @app.route("/test_data")
 def names():
