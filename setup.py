0fc47bede90426170a0066e49d712e7d0317988c
SnapTrap <45294596+fetttttjoe@users.noreply.github.com>
2022-03-27 00:04:20 +0100

    minor changes

--- setup.py
+++ setup.py
@@ -1,34 +1,35 @@
 from cx_Freeze import setup, Executable
 from datetime import date
 import os
+
+programName = "RemoteControl"
+programPath = f"{os.path.abspath('.')}/{programName}"
 #
 # Folders to include
 # Note: possible to add files if in same folder as setup.py
 #
-programName = "RemoteControl"
-programPath = f"{os.path.abspath('.')}/{programName}"
 includeFiles = [    
                 f"{programPath}/templates",
                 f"{programPath}/static", 
                 f"{programPath}/util",
                 f"{programPath}/certificates" ]
+excludePackages = ["tkinter", "test", "unittest"]
+buildOptions = {
+    'build_exe' : f'build/{programName}',
+    'excludes' : excludePackages,
+    'packages': [ 'jinja2',
+                  'jinja2.ext',
+                  'sys',
+                  'os'],
+    'include_files': includeFiles,
+    'include_msvcr': True }
 
 today = date.today()
 d = today.strftime("%m/%d/%Y")
 
 executable = Executable(script="startServer.py", target_name=f"{programName}.exe")
-
 setup(name = programName,
       version="0.1",
       description= f"RemoteControl BuildDate: {d}",
-      options={
-      'build_exe': {
-          'build_exe' : f'build/{programName}',
-          'packages': [
-                        'jinja2',
-                        'jinja2.ext',
-                        'sys',
-                        'os'],
-          'include_files': includeFiles,
-          'include_msvcr': True}},
+      options= {'build_exe': buildOptions},
       executables=[executable], requires=['flask'])
