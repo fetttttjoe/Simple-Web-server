d17b4793ca2566b8ecc24d0d6f9fdd71eb8a0421
SnapTrap <45294596+fetttttjoe@users.noreply.github.com>
2022-04-03 00:42:59 +0200

    Add files via upload

--- RemoteControl/util/stream.py
+++ RemoteControl/util/stream.py
@@ -5,35 +5,69 @@
 # https://blog.miguelgrinberg.com/post/flask-video-streaming-revisited
 # https://blog.miguelgrinberg.com/post/video-streaming-with-flask
 # 
-from cv2 import imencode # https://docs.opencv.org/4.x/d6/d00/tutorial_py_root.html
+import threading
+import cv2 as cv # https://docs.opencv.org/4.x/d6/d00/tutorial_py_root.html
 from mss import mss # https://python-mss.readthedocs.io/examples.html1
-from numpy import array
-from threading import Thread
+import numpy as np
+from threading import Thread, RLock
 import time
-
+import itertools
 class Stream(object):
-    # Class variables
-    monitor = {}
-    thread = None
-    frame = None
-    lastAccess = 0
+    # Class variables for each thread 
+    _maxStreams = 5 # fixed for now
+    _streamCount = 0 
+    classMonitor = np.full((_maxStreams), None)
+    classStreams = np.full((_maxStreams), None)
+    classFrames = np.full((_maxStreams), None)
+    classLastAccess = np.full((_maxStreams), None)
+    __lock = RLock()
     #
-    # Constructor
+    # Constructor i am sure this is not the best solution 
+    # the amount of objects and _ThreadCount are kinda the same
+    # TODO: learn about pools and redo this class
+    #   Critical: EVERY device needs 2 threads for both windows 
+    #               -> because we create objects on every call 
+    #               ->    
     #
     def __init__(self, monitor):
-        if Stream.thread is None:
-            Stream.monitor = monitor
-            Stream.lastAccess = time.time() # create a time stamp on construction
-            Stream.thread = Thread(target=self._thread) # create Thread
-            Stream.thread.start() # start Thread
-            while self.getCurrentFrame() is None:
-                time.sleep(0)
-    #
+        self.id = Stream._streamCount; 
+        while Stream._streamCount >= Stream._maxStreams: # make sure we have some kind of limit 
+            print(f"Max Objects reached! Only {Stream._maxStreams} allowed") # DEBUG
+            for num, element in enumerate(Stream.classStreams):
+                if element is None:
+                    self.id = num
+                    break
+        Stream._streamCount += 1
+        Stream.classMonitor[self.id] = monitor
+        self.monitor = monitor
+        #if monitor in Stream.classMonitor:
+        #    raise ValueError('Stream already created')
+        print("__init__ for ID:",self.id, Stream.classStreams, Stream.classStreams[self.id])
+    #    
     # Getter for current frame
     #
     def getCurrentFrame(self):
-        Stream.lastAccess = time.time() # update the time Stamp with latest Request time
-        return Stream.frame
+        print("getCurrentFrame self: ", self)
+        print(f"getCurrentFrame[{self.id}] time: ", Stream.classLastAccess[self.id], type(Stream.classFrames[self.id]))
+        #for num, element in enumerate(Stream.classFrames):                  # DEBUG
+        #    print(f"get Current Frame with type:{type(element)} at Pos:({num})")    # DEBUG
+        with Stream.__lock:
+            Stream.classLastAccess[self.id] = time.time() # update the time Stamp with latest Request time
+            return Stream.classFrames[self.id] # Return the frame from Thread
+    #
+    # Start the Stream from Outside
+    #
+    def startStream(self):
+        if Stream.classStreams[self.id] is None:
+            Stream.classMonitor[self.id] = Stream.classMonitor[self.id]
+            print("startStream object Created for:", Stream.classMonitor[self.id]) # DEBUG
+            Stream.classLastAccess[self.id] = time.time()
+            Stream.classStreams[self.id] = Thread(target=self._thread)
+            print(f"startStream Thread{self.id} created {self.classStreams[self.id]}") # DEBUG
+            print(self.classStreams) #DEBUG
+            Stream.classStreams[self.id].daemon = True
+            Stream.classStreams[self.id].start()
+    #           
     #
     # grabs the given monitor area and returns the generator 
     # monitor = {'top'      : pixel-pos,
@@ -42,35 +76,48 @@ class Stream(object):
     #            'height'   : window-height
     #           }
     # fps = default 0.5 -> a ss every 2 sek
-    #   -> if set to 0 its unlimited
+    #   -> if set to 0 its "unlimited"
+    # scalePercent = default 30 -> how much we want to scale the img
+    #       example: scalePercent=30  -> 30% of original img size
+    #   
     #
-    @staticmethod
-    def frames(monitor, fps=1):
-        if fps == 0: # make sure sleep is set to 0 -> TODO: Benchmark this and check if its useful to do more than 1 SS / sek
+    def generateFrame(self, scalePercent=30, fps:int=1):
+        if fps == 0: 
             sleepTimer = 0
         else: 
-            sleepTimer = (1 * (1 // fps)) # make even numberswith mss.mss() as sct:
+            sleepTimer = (1 * (1 // fps)) # make even numbers
         with mss() as sct:
-            while True:
-                time.sleep(sleepTimer)
-                raw = sct.grab(monitor)
-                # encode as a jpeg image  -> https://www.geeksforgeeks.org/python-opencv-imencode-function/
-                # encode image into stream data
-                # convert stream data into numpy array
-                # convert numpy array into bytes
-                bytesImg = imencode('.jpg', array(raw))[1].tobytes()
-                yield(bytesImg) # return the Generator
+            time.sleep(sleepTimer) # we only want a screenshots with given ftps
+            print(f"frameStream from Thread[{self.id}] for Monitor: {Stream.classMonitor[self.id]}") #DEBUG
+            raw = sct.grab(Stream.classMonitor[self.id]) #grab data from screen using mss
+            img = np.array(raw) # create numpy array
+            dim = ( int(img.shape[1] * scalePercent / 100) ,    # scale the img width
+                    int(img.shape[0] * scalePercent / 100))     # scale the img height
+            resized = cv.resize(img, dim, interpolation = cv.INTER_AREA) # resize the img
+            # encode as a jpeg image  -> https://www.geeksforgeeks.org/python-opencv-imencode-function/
+            # encode image into stream data
+            # convert stream data into numpy array
+            # convert numpy array into bytes
+            byteImg = cv.imencode('.jpg', resized)[1].tobytes() 
+            return byteImg # return the byteimg 
     #
-    # method for Thread
+    # method for custom Thread
     #
-    @classmethod
-    def _thread(cls, waitTimeOut = 10):
-        print('Starting camera thread. waitTimeOut: ', waitTimeOut)
-        iterFrames = cls.frames(cls.monitor) #get the frame generator
-        for frame in iterFrames:
-            cls.frame = frame #overwrite the class attrib frame
-            if time.time() - cls.lastAccess > waitTimeOut: # if there is no user interaction for waitTimeOut end thread
-                iterFrames.close() # close the generator
-                print(f'No User conntection for {waitTimeOut} seconds')
+    def _thread(self, waitTimeOut = 10):
+        print(f"Starting stream thread [{self.id}]. waitTimeOut: {waitTimeOut}") # DEBUG
+        print(f"Thread at: {self.classStreams[self.id]}") # DEBUG
+        while True:
+            frame = self.generateFrame() #get the frame 
+            with Stream.__lock: # make sure we lock ressource from access 
+                Stream.classFrames[self.id] = frame # add the frame to class attrib classFrames   
+                time.sleep(.5)
+            if time.time() - Stream.classLastAccess[self.id] > waitTimeOut: # if there is no user interaction for waitTimeOut end thread
+                print(f'No User conntection for {waitTimeOut} seconds Thread[{self.id}] will be closed')
                 break
-        cls.thread = None # close the thread
+        # make sure thread gets unloaded
+        print(Stream._streamCount) # DEBUG
+        with Stream.__lock:
+            Stream.classStreams[self.id] = None           #TODO: I can imagine a few cases where this section is going to fail redo this with class     
+            Stream.classFrames[self.id] = None
+            Stream._streamCount -= 1; #   -> Check this and keep an eye on that
+        print(Stream._streamCount, Stream.classStreams ) # DEBUG
